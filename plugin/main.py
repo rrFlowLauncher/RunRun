b4cfae73dfa0f7af91d3fba83984a7d8536a1fab
RrobertRr <rennert.robert@gmail.com>
2023-12-14 21:18:57 +0100

    first version ready for use

--- plugin/main.py
+++ plugin/main.py
@@ -1,21 +1,41 @@
+import os.path
+import subprocess
 from flox import Flox
 
+INITIAL_KEY = "Open Configuration"
 
-class Template(Flox):
+class RunRun(Flox):
     def query(self, query):
-        for _ in range(250):
+        if not self.settings:
+            self.initial_settings()
+        if query == INITIAL_KEY:
             self.add_item(
-                title=self.args,
-                subtitle=str(_)
+                title=INITIAL_KEY,
+                subtitle="{} - {}".format(self.action_keyword, INITIAL_KEY),
+                method=self.open_settings_folder
             )
+        else:
+            for key, value in self.settings.items():
+                self.add_item(
+                    title=key,
+                    subtitle=value,
+                    method=self.change_query,
+                    parameters=[value],
+                    dont_hide=True
+                )
+
+    def initial_settings(self):
+        initial_settings = {INITIAL_KEY: "{} {}".format(self.action_keyword, INITIAL_KEY)}
+        self.settings.update(initial_settings)
 
     def context_menu(self, data):
-        self.add_item(
-            title=data,
-            subtitle=data
-        )
+        pass
+
+    def open_settings_folder(self):
+        settings_path = self.settings_path.rsplit(os.path.sep, 1)[0]
+        subprocess.Popen("explorer {}".format(settings_path), shell=True)
 
 
 if __name__ == "__main__":
-    template = Template()
-    template.run()
+    run_run = RunRun()
+    run_run.run()
